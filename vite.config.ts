69e3167b7b9b1bb00eed7b85d6e3462b99a69506
aditya-srivastavaa <01aditya.srivastava@gmail.com>
2025-12-05 16:49:38 +0530

    Fix Vercel output directory + base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   resolve: {
@@ -13,8 +12,8 @@ export default defineConfig({
       '@pathFinder': path.resolve(__dirname, './src/apps/path-finder'),
     },
   },
-  base: '/algo-visualizers/',
+  base: '/',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
 });
